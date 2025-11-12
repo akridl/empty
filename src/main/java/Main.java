4f5f7b92a04c5293c50d8a2a7a16014a96da77fc
Adam Krídl <akridl@redhat.com>
2025-11-12 17:29:04 +0100

    Add compile-time dependency for Apache's commons-lang3

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.math.NumberUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {3, -1, 4, 1, 42, 5, 9};
+        int maxNumber = NumberUtils.max(array);
+
+        System.out.println("Max number is: " + maxNumber);
+    }
+}
